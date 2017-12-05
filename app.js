12ecf703040cb9431cd5b06cff77154ed2790797
Edward Endovickiy <greeny0909@gmail.com>
2017-12-05 13:09:39 +0300

    added get request with mongodb

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var MongoClient = require('mongodb').MongoClient;
+var ObjectId = require('mongodb').ObjectId;
 
 var app = express();
 var db
@@ -20,54 +21,77 @@ var items = [
     name: 'Item3'
   }
 ]
+
 //get home
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
   res.send('App API');
 });
+
 //get all items
-app.get('/items', function (req, res) {
-  res.send(items);
-});
-//get item by id
-app.get('/item/:id', function (req, res) {
-  var item = items.find(function (items) {
-    return items.id === +req.params.id;
+app.get('/items', function(req, res) {
+  db.collection('items').find().toArray(function(err, items) {
+    if (err) {
+      console.log(err);
+      res.sendStatus(500);
+    }
+    res.send(items);
   });
-  res.send(item);
 });
+
+//get item by id
+app.get('/item/:id', function(req, res) {
+  db.collection('items').findOne({_id: ObjectId(req.params.id)}, function(err, item) {
+    if (err) {
+      console.log(err);
+      return res.sendStatus(500);
+    }
+    res.send(item);
+  })
+  // var item = items.find(function(items) {
+  //   return items.id === +req.params.id;
+  // });
+  // res.send(item);
+});
+
 //post new item
-app.post('/item', function (req, res) {
+app.post('/item', function(req, res) {
   var item = {
-    id: Date.now(),
     name: req.body.name
   };
-  items.push(item);
-  res.send(item);
+
+  db.collection('items').insert(item, function(err, result) {
+    if (err) {
+      console.log(err);
+      return res.sendStatus(500);
+    }
+    res.send(item);
+  });
+
 });
+
 //put change item by id
-app.put('/item/:id', function (req, res) {
-  var item = items.find(function (items) {
+app.put('/item/:id', function(req, res) {
+  var item = items.find(function(items) {
     return items.id === +req.params.id;
   });
   item.name = req.body.name;
   res.sendStatus(200);
 });
+
 //delete item by id
-app.delete('/item/:id', function (req, res) {
+app.delete('/item/:id', function(req, res) {
   items = items.filter(function(item) {
     return item.id !== +req.body.id;
   });
   res.sendStatus(200);
 });
 
-
-
-MongoClient.connect('mongodb://localhost:27017/api', function (err, database) {
+MongoClient.connect('mongodb://localhost:27017/api', function(err, database) {
   if (err) {
     return console.log(err);
   }
   db = database;
-  app.listen(3000, function () {
+  app.listen(3000, function() {
     console.log('App listening on port 3000');
   });
 });
