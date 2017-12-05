574fa3002e9344c9e4341b1b8e21408e54650da4
Edward Endovickiy <greeny0909@gmail.com>
2017-12-05 12:06:15 +0300

    added all requests

--- app.js
+++ app.js
@@ -18,25 +18,44 @@ var items = [
     name: 'Item3'
   }
 ]
-
+//get home
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.send('App API');
 });
-
+//get all items
 app.get('/items', function (req, res) {
   res.send(items);
 });
-
+//get item by id
 app.get('/item/:id', function (req, res) {
   var item = items.find(function (items) {
     return items.id === +req.params.id;
   });
   res.send(item);
 });
-
+//post new item
 app.post('/item', function (req, res) {
-  console.log(req.body);
-  res.send(req.body);
+  var item = {
+    id: Date.now(),
+    name: req.body.name
+  };
+  items.push(item);
+  res.send(item);
+});
+//put change item by id
+app.put('/item/:id', function (req, res) {
+  var item = items.find(function (items) {
+    return items.id === +req.params.id;
+  });
+  item.name = req.body.name;
+  res.sendStatus(200);
+});
+//delete item by id
+app.delete('/item/:id', function (req, res) {
+  items = items.filter(function(item) {
+    return item.id !== +req.body.id;
+  });
+  res.sendStatus(200);
 });
 
 app.listen(3000, function () {
